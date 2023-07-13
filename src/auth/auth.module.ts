25dad1e89cfbee50677cbab424b61c1ec92bf6b3
ivankabar <alkazavr94@mail.com>
2023-07-13 20:50:12 +0200

    added jwt passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { BasicStrategy } from './strategies/basic.strategy';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 const strategies = [BasicStrategy];
 
@@ -12,6 +15,19 @@ const strategies = [BasicStrategy];
       ttl: 10,
       limit: 5,
     }),
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const secretKey = configService.get<string>('SECRET_KEY');
+        return {
+          secret: secretKey,
+          signOptions: { expiresIn: '3m' },
+        };
+      },
+    }),
+    ConfigModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, ...strategies],
